fbb852730406655e2118a41dee6560d0a5e293ff
soro09 <rosofya2021@gmail.com>
2026-06-29 16:04:50 -0400

    Update bitstream_constructor.py

--- bitstream_constructor.py
+++ bitstream_constructor.py
@@ -1,1 +1,9 @@
 class BitstreamConstructor:
+    def __init__(self, segment_list):
+      self.segments = segment_list)
+
+    def bytes(self):
+      return b"".join(seg[1] for seg in self.segments)
+
+    def bites(self):
+      return "".join(f"{byte:08b}" for byte in raw)
