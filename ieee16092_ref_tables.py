8f289acf0357257f377a2670733e996532fde05b
soro09 <rosofya2021@gmail.com>
2026-07-02 10:58:58 -0400

    Add files via upload

---
+++ ieee16092_ref_tables.py
@@ -0,0 +1,72 @@
+#  NIST-developed software is provided by NIST as a public service. You may use,
+#  copy, and distribute copies of the software in any medium, provided that you
+#  keep intact this entire notice. You may improve, modify, and create
+#  derivative works of the software or any portion of the software, and you may
+#  copy and distribute such modifications or works. Modified works should carry
+#  a notice stating that you changed the software and should note the date and
+#  nature of any such change. Please explicitly acknowledge the National
+#  Institute of Standards and Technology as the source of the software.
+#
+#  NIST-developed software is expressly provided "AS IS." NIST MAKES NO WARRANTY
+#  OF ANY KIND, EXPRESS, IMPLIED, IN FACT, OR ARISING BY OPERATION OF LAW,
+#  INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTY OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND DATA ACCURACY. NIST
+#  NEITHER REPRESENTS NOR WARRANTS THAT THE OPERATION OF THE SOFTWARE WILL BE
+#  UNINTERRUPTED OR ERROR-FREE, OR THAT ANY DEFECTS WILL BE CORRECTED. NIST DOES
+#  NOT WARRANT OR MAKE ANY REPRESENTATIONS REGARDING THE USE OF THE SOFTWARE OR
+#  THE RESULTS THEREOF, INCLUDING BUT NOT LIMITED TO THE CORRECTNESS, ACCURACY,
+#  RELIABILITY, OR USEFULNESS OF THE SOFTWARE.
+#
+#  You are solely responsible for determining the appropriateness of using and
+#  distributing the software and you assume all risks associated with its use,
+#  including but not limited to the risks and costs of program errors,
+#  compliance with applicable laws, damage to or loss of data, programs or
+#  equipment, and the unavailability or interruption of operation. This software
+#  is not intended to be used in any situation where a failure could cause risk
+#  of injury or damage to property. The software developed by NIST employees is
+#  not subject to copyright protection within the United States.
+#
+#  Author: Eugene Song <eugene.song@nist.gov>
+#  Author: Davide Pesavento <davide.pesavento@nist.gov>
+#  Author: Tyler Wong
+
+import pandas as pd
+
+# EVAL METHODS [val1, val2]:
+# 0 = compare with min, max [min, max]
+# 1 = octet count [cnt, 00]
+# 2 = bit string [len, 00]
+# 3 = boolean [00, 00]
+# 4 = hashalg list [00, 00]
+# 5 = IA5 string [minlen, maxlen]
+# 6 = UTF8 string [minlen, maxlen]
+# 7 = signer [00, 00]
+
+ieee16092_spdu_ref = [
+    ["16092.version", "16092", 1, 0, 3, 3, True],
+    ["16092.hashalg", "16092.content", 1, 4, 00, 00, True],
+    ["16092.version", "16092.tabsData", 1, 0, 3, 3, False],
+    ["16092.psid", "16092.headerInfo", 8, 0, 0, 18446744073709551615, True],
+    ["16092.generationTime64", "16092.headerInfo", 8, 0, 0, 18446744073709551615, False],
+    ["16092.3d_lat", "16092.generationLocation", 4, 0, -900000000, 900000001, False],
+    ["16092.3d_lon", "16092.generationLocation", 4, 0, -1799999999, 1800000001, False],
+    ["16092.3d_elev", "16092.generationLocation", 2, 0, -4095, 61439, False],
+    ["16092.signer", "16092.content", 1, 7, 00, 00, False],
+    # signer = "digest"
+    ["16092.hashedid", "16092.signer", 8, 1, 8, 00, False],
+    ["16092.ecc_key_x", "16092.signature", 32, 1, 32, 00, False],
+    ["16092.ecc_key_y", "16092.signature", 32, 1, 32, 00, False],
+    ["16092.s", "16092.signature", 32, 1, 32, 00, True],
+    # signer = "certificate"
+    ["16092.cert_ver", "16092.signer_certificate", 1, 0, 3, 3, False],
+    ["16092.certtype", "16092.signer_certificate", 1, 0, 0, 1, False],
+    ["16092.hashedid", "16092.issuer", 8, 1, 8, 00, False],
+    ["16092.hostname", "16092.certificateId", 7, 6, 00, 00, False],
+    ["16092.cracaId", "16092.toBeSigned", 3, 1, 3, 00, False],
+    ["16092.crl_series", "16092.toBeSigned", 2, 0, 0, 65535, False],
+    ["16092.start_validity", "16092.toBeSigned", 4, 0, 0, 2147483646, False],
+    ["16092.lifetime", "16092.toBeSigned", 2, 0, 0, 65535, False],
+    ["16092.psid", "16092.appPermission", 4, 0, 0, 4294967295, False],
+    ["16092.ecc_key_y", "16092.verificationKeyIndicator", 32, 1, 32, 00, False],
+]
+ieee16092_spdu_refdf = pd.DataFrame(ieee16092_spdu_ref, columns = ["field", "parent", "length", "eval method", "val1", "val2", "mandatory"])
